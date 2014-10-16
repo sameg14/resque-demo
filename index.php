b00e41a9a33f9e9bb532c9320c3575e28eebbcae
Samir Patel <samir.patel@lin-digital.com>
2014-10-16 16:04:48 -0500

    Initial commit

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+require('php-resque/lib/Resque.php');
+
+// Tell resque where redis lives, in this demo it is local
+Resque::setBackend('127.0.0.1:6379');
+
+// Create some data payload to send to the job, this is what gets serialized and put in redis
+$jobArgs = array(
+    'name' => 'Samir',
+    'timestamp' => strtotime('now')
+);
+
+$jobId = Resque::enqueue('default',
