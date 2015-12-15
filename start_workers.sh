edbd7e9d52917d10a445b98a33483e1f8890d2b2
Samir Patel <samir.patel@wholefoods.com>
2015-12-14 18:52:54 -0600

    Moved resque log to /var/log

--- start_workers.sh
+++ start_workers.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-COUNT=10 QUEUE=default APP_INCLUDE=/var/www/resque-demo/bootstrap.php php vendor/chrisboulton/php-resque/bin/resque >> resque.log 2>&1
+COUNT=10 QUEUE=default APP_INCLUDE=/var/www/resque-demo/bootstrap.php php vendor/chrisboulton/php-resque/bin/resque >> /var/log/resque.log 2>&1
